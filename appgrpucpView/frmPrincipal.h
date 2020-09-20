65ae77745314d8c1ea8918c6e8b91d0cad668192
Angel Arturo villavicencio muñoz <villavicenciomunoz@gmail.com>
2020-09-19 23:03:35 -0500

    Pruebas de ramas

--- appgrpucpView/frmPrincipal.h
+++ appgrpucpView/frmPrincipal.h
@@ -35,6 +35,9 @@ namespace appgrpucpView {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::Button^ button2;
 	protected:
 
 	private:
@@ -51,6 +54,10 @@ namespace appgrpucpView {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
@@ -62,14 +69,47 @@ namespace appgrpucpView {
 			this->button1->Text = L"Ingresar";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->checkBox1);
+			this->groupBox1->Controls->Add(this->button2);
+			this->groupBox1->Location = System::Drawing::Point(89, 65);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(633, 206);
+			this->groupBox1->TabIndex = 1;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"groupBox1";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(98, 163);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 0;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(98, 70);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(80, 17);
+			this->checkBox1->TabIndex = 1;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
 			// frmPrincipal
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(897, 530);
+			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->button1);
 			this->Name = L"frmPrincipal";
 			this->Text = L"frmPrincipal";
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
