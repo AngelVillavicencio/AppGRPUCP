a477c67c9abb70421ecfce6bfc56694dc572f70f
Angel Arturo villavicencio muñoz <villavicenciomunoz@gmail.com>
2020-09-19 22:50:06 -0500

    Base del proyecto

---
+++ appgrpucpView/frmPrincipal.h
@@ -0,0 +1,78 @@
+#pragma once
+
+namespace appgrpucpView {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de frmPrincipal
+	/// </summary>
+	public ref class frmPrincipal : public System::Windows::Forms::Form
+	{
+	public:
+		frmPrincipal(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~frmPrincipal()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(663, 430);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(139, 41);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Ingresar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// frmPrincipal
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(897, 530);
+			this->Controls->Add(this->button1);
+			this->Name = L"frmPrincipal";
+			this->Text = L"frmPrincipal";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
